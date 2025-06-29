6fb1c7a2f9b917ba378ba91ca36973da6e34095b
dev04sa <devsa2067@gmail.com>
2025-06-29 15:03:31 +0530

    updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,31 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+// 	plugins: [react()],
+// 	server: {
+// 		port: 3000,
+// 		// Get rid of the CORS error
+// 		proxy: {
+// 			"/api": {
+// 				target: "https://loopin-backend-an46.onrender.com",
+// 				changeOrigin: true,
+// 				secure: false,
+// 			},
+// 		},
+// 	},
+// });
+
+
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-		// Get rid of the CORS error
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-				changeOrigin: true,
-				secure: false,
-			},
-		},
-	},
+  plugins: [react()],
+  server: {
+    port: 3000, // ✅ keep your dev port
+  },
 });
+
