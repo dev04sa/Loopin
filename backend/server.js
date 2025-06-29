ec416cd7a7ded9e30a5e372332c3e204c2207800
dev04sa <devsa2067@gmail.com>
2025-06-29 22:44:12 +0530

    cors updated

--- backend/server.js
+++ backend/server.js
@@ -51,11 +51,11 @@ app.use("/api/messages", messageRoutes);
 // if (process.env.NODE_ENV === "production") {
 //   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  // react app
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
+//   // react app
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+//   });
+// }
 
 server.listen(PORT, () =>
   console.log(`Server started at http://localhost:${PORT}`),
